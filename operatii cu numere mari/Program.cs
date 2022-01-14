ef7b43b7662b02d921488c59f971da9a8365ec77
Robert S <robert.srg02@gmail.com>
2022-01-14 16:15:52 +0200

    Add project files.

---
+++ operatii cu numere mari/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace operatii_cu_numere_mari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
